efa842342420a9848f8f56d4abd46e918d0fea15
pcrane <trigunshin@gmail.com>
2010-03-29 10:22:20 -0400

    Added auto-spy functionality; the planet's name does need to be hardcoded in the auto-espionage file however.  There is no validation as far as shipcount/fleetslots go, either.

---
+++ auto_espionage.py
@@ -0,0 +1,23 @@
+import weblogic
+import planets
+import galaxy
+
+class Espionage:
+    
+    def __init__(self):
+        pass
+    
+if __name__ == '__main__':
+    wl = weblogic.Weblogic()
+    ld = wl.login()
+    scanner = galaxy.GalaxyScanner()
+    planetManager = planets.PlanetManager(wl)
+    planetList = planets.PlanetManager.availablePlanets(planetManager, wl.getRecentResponse())
+    planetList.append(planetManager.currentPlanet(wl.getRecentResponse()))
+    targetPlanet = "Pantomime"
+    for planet in planetList:
+        print planet.name
+        if str(planet.name) == targetPlanet:
+            planet.switch("overview",ld.session)
+            loc = planet.location
+            scanner.scanSystems(wl, ld, loc.galaxy, loc.solarsystem, 20)
