91fb3c0c7b2486ba487dd6e3edf22f2e7ff968ac
pcrane <trigunshin@gmail.com>
2010-03-31 02:22:46 -0400

    Updated for DST.

--- header.py
+++ header.py
@@ -64,7 +64,7 @@ class FlightInfo:
 
     def __init__(self, incHour, incMin, incSec, descrip, mission, incOrigin, incGal, incSys, incSlot, incDetails, tgtName, tgtGal, tgtSys, tgtSlot):
         #fix hour for GMT
-        self.arrival_hour=str(int(incHour)-5)
+        self.arrival_hour=str(int(incHour)-6)
         if int(self.arrival_hour) < 0:
             self.arrival_hour = int(self.arrival_hour) + 24
         self.arrival_minute=incMin
